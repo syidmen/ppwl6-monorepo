25ac4c0e45a170532cad76c19a214bc395efa6d7
syidmen <rasyidmuh0907@gmail.com>
2026-03-15 22:03:02 +0700

    penyesuaian main.tsx ke /app2

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
 // import App from './ApiTest'
-// import App from './App2'
+import App from './App2'
 
 
 createRoot(document.getElementById('root')!).render(
